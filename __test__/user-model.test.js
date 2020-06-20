80ab4399fb024c2960fad48e7aedf800faecaef5
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-06-20 22:12:49 +0300

    category search

--- __test__/user-model.test.js
+++ __test__/user-model.test.js
@@ -5,10 +5,13 @@ const users = require('../lib/models/users-model.js');
 
 describe('users Model', () => {
   const obj = { username : 'yasmin' , password: '1234'};
+ 
+
   it('create method (post)', () => {
     return users.create(obj).then((result) => {
       Object.keys(obj).forEach((key) => {
-        expect(result[key]).toEqual(obj[key]);
+        expect(result.username).toEqual('yasmin');
+        expect(result.password).toEqual(users.generateToken(obj));
       });
     });
   });
